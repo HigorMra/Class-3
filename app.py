b346738ad42816f5703d06182447af1353f6e631
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2024-04-16 16:14:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+num1 = 5
+num2 = 3
+
+print(type(num1))
+print(type(num2))
+
+a = float(num1)
+print(a)
+print(type(a))
+
+b = int(num2)
+print(b)
+print(type(b))
+
+a = int(float("5"))
+b = "3.5"
+
+print(type(a))
+
+
+(num1 + num2)
+(num1 - num2)
+(num1 * num2)
+(10 / 3)
+(10 // 3)
+(10 % 3)
+(2 ** 3)
+
+(3 + 5 * 7 + 3)
+(3 + 7) * (7 + 3)
+
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,85,17,-55,5))
+print(min(1,85,17,-55,5))
+print(round(8.3))
+print(round(8.8))
+
+import math
+
+print(math.floor(8.999))
+print(math.ceil(8.999))
+print(math.sqrt(9))
